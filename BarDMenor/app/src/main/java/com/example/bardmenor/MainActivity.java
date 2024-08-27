0ace119228b515409297286e333e78cc9c8ffea7
Guilherme Sampaio <g.sampaio2021@gmail.com>
2024-08-26 23:30:46 -0300

    Some changes.
    I changed:
    - App icon.
    - Introduction screen with a "Toast" message.
    - Background color: "#141e41" used in "ActivityMain".
    - Used "getWindow().setStatusBarColor(Color.parseColor("#141e41"));" to take a clean visual.
    
    These are some updates. Tomorrow will be a new day... ;)

--- BarDMenor/app/src/main/java/com/example/bardmenor/MainActivity.java
+++ BarDMenor/app/src/main/java/com/example/bardmenor/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.bardmenor;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +20,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        getWindow().setStatusBarColor(Color.parseColor("#141e41"));
+
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, FormLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3400);
+
+        Toast.makeText(this, "Bem vindo ao Bar De menores!", Toast.LENGTH_LONG).show();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Bar-DMenor
  (Projeto) - Aplicativo usando Kotlin e Java para apresentar um projeto em desenvolvimento, chamado: "Bar DMenor".
+
+ - Versão Beta 1.0 -
+
+ Tasks para serem concluídas:
+ * Colocar uma animação de transição da "MainActivity" para a "FormLogin".
+ * Setar um ícone para a aplicação. - (FEITO)
+ * Criar o FrontEnd da tela de Login.
+ * Criar o banco de dados do Sistema de login.
+ * Fazer um Sistema de Login funcional e com conexão via dB.
+
+ - Versão Beta 2.0 -
+
+ Tasks para serem concluídas:
+ * Pensar como será a tela principal.
+ * Fazer uma side bar para navegação de multiplos forms.
+ * Adicionar animações.
+ * Prezar a optimização.
+
+ É isso até então. 
